f665f05504135dee090da016d3a9ad92a940f492
DroidStudioAS <as.droiddevteam@gmail.com>
2024-03-05 11:55:37 +0100

    added initial stylling to contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,9 +5,17 @@
 @endsection
 @extends("legend")
 @section("content")
-    <h1>
-        This Is A Contact Page
-    </h1>
+    <div class="content-container">
+        <h1>
+            This Is A Contact Page
+        </h1>
+        <form class="contact-form">
+            <input class="subject-input" type="text" placeholder="subject">
+            <input class="email-input" type="email" placeholder="email">
+            <textarea placeholder="Your Message"></textarea>
+            <input class="submit" type="submit">
+        </form>
+    </div>
 @endsection
 
 </html>
