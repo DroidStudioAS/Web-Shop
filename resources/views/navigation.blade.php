78551f05e35561cb5433071dfd490b19b9789b0e
DroidStudioAS <as.droiddevteam@gmail.com>
2024-03-07 14:25:43 +0100

    addded product category cards and website logo

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -7,6 +7,7 @@
     <nav class="nav-container">
         <a href="/" class="nav-item">Home</a>
         <a href="/contact" class="nav-item">Contact</a>
+        <img src="{{ asset("logo.png") }}"/>
         <a href="/shop" class="nav-item">Shop</a>
         <a href="/about" class="nav-item">About</a>
     </nav>
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
 @extends('legend')
 @section('title')
     Shop
@@ -10,12 +12,41 @@
         </h1>
         <div class="category-container">
             @foreach($categories as $category)
-                <div class="product_category">
-                    {{$category->category_name}}
+                <div class="product-category">
+                    <h3>{{$category->category_name}}</h3>
+                    <img src="{{ asset('tn.png') }}" alt="placeholder">
+                    <input type="submit" class="submit"/>
                 </div>
             @endforeach
         </div>
 
     </div>
 @endsection
+</html>
+<style>
+    .category-container{
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-evenly;
+        align-items: center;
+        width: 80vw;
+        padding: 20px;
+    }
+    .product-category{
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        flex-flow: column nowrap;
+        font-size: xx-large;
+        border:2px white solid;
+        border-radius: 8px 6px 8px 6px;
+        padding: 20px;
+        width: 20vw;
+
+    }
+    .product-category img{
+        margin: 30px;
+        width: 15vw;
+    }
+</style>
 
